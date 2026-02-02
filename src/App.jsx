636b622361cf063ccfe24ddeb05c7e65677983b4
GBBk <benjaminbernardi43@gmail.com>
2026-02-02 16:11:43 -0300

    cat peliculas

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import "./App.css";
+import { Movies } from "./components/Movies";
+import { useMovies } from "./hooks/useMovies";
+import { useState, useEffect, useRef } from "react";
+
+function useSearch() {
+  const [search, setUpdateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+
+    if (search === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError("No se puede buscar una película con un número");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, setUpdateSearch, error };
+}
+
+function App() {
+  const { search, setUpdateSearch, error } = useSearch();
+  const [sort, setSort] = useState(false);
+  const { movies, getMovies, loading } = useMovies({ search, sort });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMovies({ search });
+  };
+
+  const handleChange = (e) => {
+    setUpdateSearch(e.target.value);
+  };
+
+  const handleSort = () => {
+    setSort(!sort);
+  };
+
+  return (
+    <div className="page">
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: "1px solid transparent",
+              borderColor: error ? "red" : "transparent",
+            }}
+            onChange={handleChange}
+            value={search}
+            name="query"
+            placeholder="Avengers, Star Wars, The Matrix..."
+          />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
+          <button type="submit">Buscar</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </header>
+      <main>
+        {loading ? (
+          <p className="loading">Loading...</p>
+        ) : (
+          <Movies movies={movies} />
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
